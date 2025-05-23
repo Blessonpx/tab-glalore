02a88d8cb03aec1a2864d01c0d85d6f2ba82c7e2
Blessonpx <blessonxavierofficial@gmail.com>
2025-05-23 12:54:50 +0530

    Repo Manipulation in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
-<script setup>
+<script>
+import { ref, onMounted } from 'vue'
+import config from './config.js'
+
+
+export default {
+  setup() {
+    const repos = ref([])
+
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch(`${config.apiBaseUrl}/eg/getRepos`)
+        const data = await response.json()
+
+        // Filter only repos that match names in config.defaultRepos
+        const filtered = data.filter(repo =>
+          config.defaultRepos.includes(repo.name)
+        )
+        repos.value = filtered
+        // repos.value = data
+        console.log('Fetched Repos:', data)
+      } catch (error) {
+        console.error('Failed to fetch repos:', error)
+      }
+    }
+
+    onMounted(() => {
+      fetchRepos()
+    })
+
+    return {
+      repos
+    }
+  }
+}
 </script>
 
 <template>
@@ -9,6 +43,18 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+  </div>
+
+  <div>
+    <h2>Git Repositories</h2>
+
+    <ul v-if="repos.length > 0">
+      <li v-for="repo in repos" :key="repo.id">
+        <strong>{{ repo.name }}</strong><br />
+      </li>
+    </ul>
+
+    <p v-else>Loading repositories...</p>
   </div>
 </template>
 
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default{
+  apiBaseUrl: 'http://localhost:9002',
+  defaultRepos: ['jenkins_test_1', 'diyui']
+}
